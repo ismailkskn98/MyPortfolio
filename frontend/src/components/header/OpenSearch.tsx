fc1400cbe9ee135ba28707cc65e8e38c88cfd700
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-05-03 04:52:29 +0300

    frontend: Search kapanış animasyonu eklendi.

--- frontend/src/components/header/OpenSearch.tsx
+++ frontend/src/components/header/OpenSearch.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import SearchList from "./SearchList";
 import { IoSearchSharp } from "react-icons/io5";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 type Props = {
   openSearch: boolean;
@@ -12,39 +12,42 @@ type Props = {
 const OpenSearch: React.FC<Props> = ({ openSearch, toggleSearch }) => {
   return (
     <>
-      {openSearch && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          id="searchBodyWrapper"
-          onClick={toggleSearch}
-          className="w-full h-screen absolute top-0 left-0 z-10 bg-[#0000]/70 flex items-start justify-center pt-52"
-        >
+      <AnimatePresence>
+        {openSearch && (
           <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{ duration: 0.3 }}
-            id="searchListWrapper"
-            className="bg-White rounded-[32px] px-5 py-3 w-[35rem] flex flex-col items-center justify-center"
-            onClick={(e) => e.stopPropagation()}
+            exit={{ opacity: 0 }}
+            id="searchBodyWrapper"
+            onClick={toggleSearch}
+            className="w-full h-screen absolute top-0 left-0 z-10 bg-[#0000]/70 flex items-start justify-center pt-52"
           >
-            <div
-              id="search"
-              className="w-full relative h-12 flex items-center justify-center gap-3"
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.3 }}
+              id="searchListWrapper"
+              className="bg-White rounded-[32px] px-5 py-3 w-[35rem] flex flex-col items-center justify-center"
+              onClick={(e) => e.stopPropagation()}
             >
-              <IoSearchSharp className="w-[23px] h-[23px] text-BG1/50 font-bold" />
-              <input
-                type="search"
-                placeholder="Ara..."
-                className="w-full border-none outline-none search-u text-BG1/70 tracking-wider"
-              />
-            </div>
-            <div id="searchLine" className="w-full h-[1px] bg-BG1/5 px-5"></div>
-            <SearchList />
+              <div
+                id="search"
+                className="w-full relative h-12 flex items-center justify-center gap-3"
+              >
+                <IoSearchSharp className="w-[23px] h-[23px] text-BG1/50 font-bold" />
+                <input
+                  type="search"
+                  placeholder="Ara..."
+                  className="w-full border-none outline-none search-u text-BG1/70 tracking-wider"
+                />
+              </div>
+              <div id="searchLine" className="w-full h-[1px] bg-BG1/5 px-5"></div>
+              <SearchList />
+            </motion.div>
           </motion.div>
-        </motion.div>
-      )}
+        )}
+      </AnimatePresence>
     </>
   );
 };
