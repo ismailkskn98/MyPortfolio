f9bbc6b0fc5c236749d884698f6c47813ddb49ba
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-05-10 19:18:14 +0300

    frontend: Works.tsx tailwind css karmaşıklığa yol açtığı için ayrı bir dosyaya yerleştirildi.

---
+++ frontend/src/components/works/WorksContent.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { StaticImageData } from "next/image";
+import React, { useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import tuzgoluHorizontal from "@/../public/images/works/tuzgolumotorhorizontal.png";
+import tuzgoluVertical from "@/../public/images/works/tuzgolumotorvertical.png";
+import renatoHorizontal from "@/../public/images/works/renatohorizontal.png";
+import renatoVertical from "@/../public/images/works/renatovertical.png";
+import styles from "./styles.module.css";
+import WorksSlider from "./WorksSlider";
+
+export type SliderItem = {
+  link: string;
+  verticalImage: StaticImageData;
+  horizontalImage: StaticImageData;
+};
+
+const WorksContent = () => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const sliderItems: SliderItem[] = [
+    {
+      link: "https://www.tuzgolumotorluaraclar.com/",
+      verticalImage: tuzgoluVertical,
+      horizontalImage: tuzgoluHorizontal,
+    },
+    {
+      link: "https://ryonetim-nuxtjs.vercel.app/",
+      verticalImage: renatoVertical,
+      horizontalImage: renatoHorizontal,
+    },
+  ];
+
+  const handleClickLeft = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? sliderItems.length - 1 : prevIndex - 1));
+  };
+
+  const handleClickRight = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === sliderItems.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div id="islerContent" className="flex items-center gap-0 lg:gap-16">
+      <div id="islerContentButtonLeft" className={styles.sliderBtnLeft} onClick={handleClickLeft}>
+        <IoIosArrowBack className="w-3 h-3 md:w-10 md:h-10" />
+      </div>
+      <WorksSlider sliderItems={sliderItems} currentIndex={currentIndex} />
+      <div
+        id="islerContentButtonRight"
+        className={styles.sliderBtnRight}
+        onClick={handleClickRight}
+      >
+        <IoIosArrowForward className="w-3 h-3 md:w-10 md:h-10" />
+      </div>
+    </div>
+  );
+};
+
+export default WorksContent;
---
+++ frontend/src/components/works/WorksSlider.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import dualScreen from "@/../public/images/works/Dual-screen.png";
+import Link from "next/link";
+import { RiExternalLinkLine } from "react-icons/ri";
+import Image from "next/image";
+import styles from "./styles.module.css";
+import { SliderItem } from "./WorksContent";
+
+type Props = {
+  sliderItems: SliderItem[];
+  currentIndex: number;
+};
+
+const WorksSlider: React.FC<Props> = ({ sliderItems, currentIndex }) => {
+  return (
+    <div id="islerContentSlider">
+      <div className="relative">
+        {sliderItems.map((item, i) => (
+          <div key={i} className={`w-full absolute top-0 left-0`}>
+            <Link
+              href={item.link}
+              className={`${styles.sliderLink} code-m lg:menu-m ${
+                i === currentIndex ? "block" : "hidden"
+              }`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Siteyi Görüntüle <RiExternalLinkLine className="w-4 h-4" />
+            </Link>
+            <div className={styles.sliderVertical}>
+              <Image
+                src={item.verticalImage}
+                alt="image"
+                className={`w-full h-full object-cover absolute top-0 left-0 transition-opacity ${
+                  i === currentIndex ? "opacity-100" : "opacity-0"
+                }`}
+              />
+            </div>
+            <div className={styles.sliderHorizontal}>
+              <Image
+                src={item.horizontalImage}
+                alt="image"
+                className={`w-full h-full object-cover absolute top-0 left-0 transition-opacity ${
+                  i === currentIndex ? "opacity-100" : "opacity-0"
+                }`}
+              />
+            </div>
+          </div>
+        ))}
+        <Image
+          src={dualScreen}
+          alt="yapılan işler"
+          className="w-[263px] h-[198px] md:w-[480px] md:h-[320px] lg:w-[650px] lg:h-[432px]"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default WorksSlider;
