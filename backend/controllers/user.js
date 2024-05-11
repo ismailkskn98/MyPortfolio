d860e13213ab32bd7d1f5b404c3643a3d211f4c8
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-05-11 18:56:03 +0300

    backend: /blogs/lastblog route eklendi. Controllerı yazıldı.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -49,3 +49,26 @@ exports.get_blog_by_slug = async (req, res) => {
         res.status(500).send({message: 'Sunucuda bir hata oluştu. Lütfen daha sonra tekrar deneyin veya yöneticiye başvurun.'});
     }
 }
+
+exports.get_last_blog = async (req, res) => {
+    try {
+        const lastBlog = await Blog.findOne({
+            order: [['id', 'DESC']],
+            attributes: ['title', 'subtitle', 'slug', 'createdAt'],
+            include: [{
+                    model: Category,
+                    attributes: ['name'],
+                }, {
+                    model: User,
+                    attributes: ['firstname'],
+                }
+            ]
+        })
+        if(!lastBlog) {
+          return res.status(404).json({message: 'blog bulunamadı. Lütfen daha sonra tekrar deneyin.'});
+        }
+        return res.json(lastBlog);
+    } catch (error) {
+        res.status(500).json({message: 'Sunucuda bir hata oluştu. Lütfen daha sonra tekrar deneyin veya yöneticiye başvurun.'});
+    }
+} 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.js');
 
+router.get('/blogs/lastblog', userController.get_last_blog);
 router.get('/blogs/:slug', userController.get_blog_by_slug);
 router.get('/blogs', userController.get_blogs);
 
