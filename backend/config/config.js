496dd21a2d032d6730c7efc21ef01f6d7064fa43
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-07-13 21:57:47 +0300

    sequelize config update

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const dotenv = require('dotenv');
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local';
-dotenv.config({path: path.resolve(__dirname, '..' ,envFile)});
+const path = require("path");
+const dotenv = require("dotenv");
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.local";
+dotenv.config({ path: path.resolve(__dirname, "..", envFile) });
 
 const config = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: "127.0.0.1",
-    dialect: "mysql"
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: "127.0.0.1",
-    dialect: "mysql"
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
 };
 
 module.exports = config;
