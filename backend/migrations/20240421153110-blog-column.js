2e99755a8d6a7a488ccf2faff520b141f57a0704
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-07-16 00:56:10 +0300

    sequelize migration update

--- backend/migrations/20240421153110-blog-column.js
+++ backend/migrations/20240421153110-blog-column.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.addColumn('blogs', 'userId', {
-        type: Sequelize.DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: {
-            tableName: 'users',
-          },
-          key: 'id',
+  async up(queryInterface, Sequelize) {
+    return queryInterface.addColumn("blog", "userId", {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: "users",
         },
-      },)
+        key: "id",
+      },
+    });
   },
-  async down (queryInterface, Sequelize) {
-    return queryInterface.removeColumn('blogs', 'userId');
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.removeColumn("blog", "userId");
+  },
 };
