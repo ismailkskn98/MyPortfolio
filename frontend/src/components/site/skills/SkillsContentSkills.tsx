a6185df98e92fae2b5ac4ff08c04804d9c789570
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-07-30 00:29:11 +0300

    hata düzeltildi

--- frontend/src/components/site/skills/SkillsContentSkills.tsx
+++ frontend/src/components/site/skills/SkillsContentSkills.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import type { SkillsType } from "@/app/(admin)/admin/yetenekler/page";
 import Image from "next/image";
+import { SkillsType } from "@/components/admin/skills";
 
 // http://localhost:8080
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -20,16 +20,8 @@ const SkillsContentSkills = ({ skills }: { skills: SkillsType[] }) => {
     <div className="grid grid-cols-2 grid-rows-3 lg:grid-cols-4 lg:grid-rows-2 gap-x-16 sm:gap-x-24 lg:gap-x-32 gap-y-8 lg:gap-y-16">
       {skills.map((item: SkillsType, i) => (
         <div key={i} className="flex flex-col items-center gap-6">
-          <span
-            className={`flex items-center justify-center w-36 h-36 rounded-full relative bg-React`}
-          >
-            <Image
-              src={`${BASE_URL}/${item.image}`}
-              alt={item.name}
-              width={100}
-              height={100}
-              className="w-20 h-20"
-            />
+          <span className={`flex items-center justify-center w-36 h-36 rounded-full relative bg-React`}>
+            <Image src={`${BASE_URL}/${item.image}`} alt={item.name} width={100} height={100} className="w-20 h-20" />
           </span>
           <p className={`h2-m uppercase text-white`}>{item.name}</p>
         </div>
