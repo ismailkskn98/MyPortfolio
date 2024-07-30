c7ba7c7793b5bbde2e4c4b153ab7c75668c04444
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-07-30 15:33:49 +0300

    hata düzeltildi

--- frontend/src/components/login/index.tsx
+++ frontend/src/components/login/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import LoginLeft from "./LoginLeft";
 import LoginRight from "./LoginRight";
 
@@ -6,7 +6,9 @@ const LoginContainer = () => {
   return (
     <section className="w-full h-full flex flex-row items-center overflow-hidden ">
       <LoginLeft />
-      <LoginRight />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginRight />
+      </Suspense>
     </section>
   );
 };
