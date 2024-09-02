97da585d8cc581c9236a17270c6f1794db8f464f
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-09-03 00:59:15 +0300

    skills component update

--- frontend/src/components/site/skills/SkillsContentServices.tsx
+++ frontend/src/components/site/skills/SkillsContentServices.tsx
@@ -5,7 +5,6 @@ import { IoCode } from "react-icons/io5";
 type ContentItem = {
   title: string;
   text: string;
-  color: string;
   icon: React.ElementType;
 };
 
@@ -13,13 +12,11 @@ const contentItems: ContentItem[] = [
   {
     title: "Front-End Developer",
     text: "HTML·CSS·JS·REACT",
-    color: "bg-CSS",
     icon: RiComputerLine,
   },
   {
     title: "Backend Developer",
     text: "Express.js·MySQL·Sequelize",
-    color: "bg-React",
     icon: IoCode,
   },
 ];
@@ -29,7 +26,7 @@ const SkillsContentServices = () => {
     <div id="skillsServices" className="flex items-center gap-8 lg:gap-32 flex-col md:flex-row">
       {contentItems.map((item, i) => (
         <div key={i} className="flex items-center gap-0">
-          <div className={`${item.color} rounded-l-lg w-2 min-h-[132px]`}></div>
+          <div className={`skills-content rounded-l-lg w-2 min-h-[132px]`}></div>
           <div className="flex flex-col items-center gap-2 min-h-[132px] px-6 py-4 rounded-r-lg bg-Brand2">
             <item.icon className="w-8 h-8" />
             <div className="flex flex-col items-center gap-2">
--- frontend/src/components/site/skills/SkillsContentSkills.tsx
+++ frontend/src/components/site/skills/SkillsContentSkills.tsx
@@ -12,7 +12,7 @@ const SkillsContentSkills = ({ skills }: { skills: SkillType[] }) => {
     <div className="grid grid-cols-2 grid-rows-3 lg:grid-cols-4 lg:grid-rows-2 gap-x-16 sm:gap-x-24 lg:gap-x-32 gap-y-8 lg:gap-y-16">
       {skills.map((item: SkillType, i) => (
         <div key={i} className="flex flex-col items-center gap-6">
-          <span className={`flex items-center justify-center w-36 h-36 rounded-full relative bg-React`}>
+          <span className={`skills flex items-center justify-center w-36 h-36 rounded-full relative bg-React`}>
             <Image src={`${BASE_URL}/${item.image}`} alt={item.name} width={100} height={100} className="w-20 h-20" />
           </span>
           <p className={`h2-m uppercase text-white`}>{item.name}</p>
