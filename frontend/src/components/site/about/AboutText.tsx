98ceb7c31dc790ea04449f7df52a0502d356fa5f
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-09-06 18:04:49 +0300

    about update

--- frontend/src/components/site/about/AboutText.tsx
+++ frontend/src/components/site/about/AboutText.tsx
@@ -5,10 +5,10 @@ import type { AboutType } from "@/types";
 
 const AboutText = ({ about }: { about: AboutType[] }) => {
   return (
-    <div id="aboutText" className="flex flex-col gap-4 px-6 sm:px-10 py-6 bg-BG1 rounded-[40px]">
+    <div id="aboutText" className="sm:min-w-[31rem] basis-[75rem] flex flex-col gap-4 px-6 sm:px-10 py-6 bg-BG1 rounded-[40px]">
       <p className="code-m text-Brand2">{"<p>"}</p>
       <p className="block logo-m text-Brand1">Selam!</p>
-      <div className="text-white para-m">{parse(about[0].about)}</div>
+      <div className="text-white para-m leading-7">{parse(about[0].about)}</div>
       <p className="code-m text-Brand2">{"</p>"}</p>
     </div>
   );
