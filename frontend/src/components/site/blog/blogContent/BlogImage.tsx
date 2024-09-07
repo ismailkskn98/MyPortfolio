9c1b4b3a8b8407fc1b075c98522b536b1340eae7
İsmail Keskin <hamdismailkskn98@gmail.com>
2024-09-07 22:45:22 +0300

    blogImage update

--- frontend/src/components/site/blog/blogContent/BlogImage.tsx
+++ frontend/src/components/site/blog/blogContent/BlogImage.tsx
@@ -12,18 +12,14 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const BlogImage: React.FC<BlogImageProps> = ({ title, image }) => {
   return (
     <div className="relative w-full h-96 rounded-sm shadow-lg">
-      {image ? (
-        <Image
-          src={`${BASE_URL}/${image}`}
-          alt={title}
-          fill
-          className="rounded-sm shadow-sm object-cover resize"
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOUklN+DwACAQFMqoz9TAAAAABJRU5ErkJggg=="
-        />
-      ) : (
-        <Image src={blogImage} alt={title} fill className="rounded-sm shadow-sm" />
-      )}
+      <Image
+        src={`${BASE_URL}/${image}`}
+        alt={title}
+        fill
+        className="rounded-sm shadow-sm object-cover resize"
+        placeholder="blur"
+        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOUklN+DwACAQFMqoz9TAAAAABJRU5ErkJggg=="
+      />
     </div>
   );
 };
